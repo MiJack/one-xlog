ceb1a17cdf83a80614de42c108f5e55f3decf732
Mi&Jack <mijackstudio@gmail.com>
2018-06-20 16:00:01 +0800

    add code

---
+++ lib/src/main/java/com/mijack/LogWriter.java
@@ -0,0 +1,14 @@
+package com.mijack;
+
+/**
+ * The log file writer
+ *
+ * @author Mi&Jack
+ */
+public class LogWriter {
+
+    public static synchronized void d(int hookId, int pid, int threadId, String msg) {
+        // notice : empty implement, the log will be outputed by xposed modelu
+    }
+
+}
